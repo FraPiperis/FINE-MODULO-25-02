3a5296819d8d5ffdc35bb2bb8d32ee51230058cd
FraPiperis <FrancescaPiperis_00@outlook.it>
2025-03-02 13:46:41 +0100

    mod 12

--- Backoffice.js
+++ Backoffice.js
@@ -1,6 +1,8 @@
 const linkAPI = 'https://striveschool-api.herokuapp.com/api/product/'
 
-const tab = async() => {
+Totale = []; 
+
+async function tab() {
     try {
         const response = await fetch(linkAPI, {
             headers: { Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2N2JlMGUxNDFlMTQwNjAwMTUzMTRkNjkiLCJpYXQiOjE3NDA1MDg2OTIsImV4cCI6MTc0MTcxODI5Mn0.DpwITaKCU7rZ2Fv20-5UysSwfeXKtUy3JTj_pXqAMyA" }
@@ -10,9 +12,7 @@ const tab = async() => {
         Totale = Prendi; 
         console.log(Prendi); 
       }
-      catch (err) {
-        console.log(err)
-    }; 
+      catch (err) {console.log(err)}; 
   }
   tab(); 
 
@@ -25,7 +25,7 @@ const tab = async() => {
     Risultato.append(...tabella); 
 }
 
-function NuovaRiga({name, description, brand, price}){
+function NuovaRiga({name, description, brand, price, _id}){
     const RigaUtente = document.createElement("tr", "table-dark")
 
      const NameUs = document.createElement("td","table-dark")
@@ -44,24 +44,43 @@ function NuovaRiga({name, description, brand, price}){
      priceUs.innerText= price; 
      RigaUtente.appendChild(priceUs); 
 
-     const deleteButton = document.createElement("btn")
-     deleteButton.classList.add("btn", "btn-warning", "m-3"); 
-     deleteButton.innerText = "Cancella prodotto"; 
-     RigaUtente.appendChild(deleteButton); 
+     const cellActions = document.createElement('td');
 
-     const EditButton = document.createElement("btn")
-     EditButton.classList.add("btn", "btn-warning"); 
-     EditButton.innerText = "Modifica prodotto"; 
-     RigaUtente.appendChild(EditButton); 
+     const editbutton = document.createElement("button")
+     editbutton.classList.add("btn", "btn-warning", "mx-2")
+     editbutton.innerHTML = '<i class="fa-regular fa-pen-to-square"></i>'
+     editbutton.addEventListener("click", () => {
+         window.location.href = `Backoffice.html?id=${_id}`
+     })
 
+    const removebutton = document.createElement("button")
+    removebutton.classList.add("btn", "btn-danger", "m-2")
+    removebutton.innerHTML = '<i class="fa-regular fa-trash-can"></i>'
+    removebutton.addEventListener("click", () => {
+        if (confirm("Sei sicuro di voler eliminare il prodotto?")){
+          deleteProduct(_id)
+        }; 
+        })
 
+    cellActions.append(editbutton, removebutton)
+    RigaUtente.append(NameUs, descriptionUs, priceUs, brandUs, cellActions)
+    
      return RigaUtente;
+}; 
+
+async function deleteProduct(id) {
+  try {
+      const res = await fetch(linkAPI + id, {
+          method: "DELETE",
+          headers: {
+              Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2N2JlMGUxNDFlMTQwNjAwMTUzMTRkNjkiLCJpYXQiOjE3NDA1MDg2OTIsImV4cCI6MTc0MTcxODI5Mn0.DpwITaKCU7rZ2Fv20-5UysSwfeXKtUy3JTj_pXqAMyA"
+          }
+      })
+      tab(); 
+  }
+  catch(err) {console.log(err)}; 
 }
 
 tab(); 
-
-
-
-
-
+ 
 
