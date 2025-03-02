377798ece493fac6ae2ac2d715fd82802b1db08c
FraPiperis <FrancescaPiperis_00@outlook.it>
2025-03-02 20:03:33 +0100

    mod 13

--- Backoffice.js
+++ Backoffice.js
@@ -1,5 +1,14 @@
 const linkAPI = 'https://striveschool-api.herokuapp.com/api/product/'
 
+const tbody = document.getElementById("tbody")
+const inputName = document.getElementById("post-name")
+const inputBrand = document.getElementById("post-brand")
+const inputPrice = document.getElementById("post-price")
+const inputImage = document.getElementById("post-image")
+const inputDesc = document.getElementById("post-description")
+const failureAlert = document.getElementById("failure")
+const successAlert = document.getElementById("success")
+
 Totale = []; 
 
 async function tab() {
@@ -47,27 +56,28 @@ function NuovaRiga({name, description, brand, price, _id}){
      const cellActions = document.createElement('td');
 
      const editbutton = document.createElement("button")
-     editbutton.classList.add("btn", "btn-warning", "mx-2")
+     editbutton.classList.add("btn", "btn-warning", "mx-2") 
      editbutton.innerHTML = '<i class="fa-regular fa-pen-to-square"></i>'
      editbutton.addEventListener("click", () => {
-         window.location.href = `Backoffice.html?id=${_id}`
+         window.location.href = `edit.html?id=${_id}`
      })
 
     const removebutton = document.createElement("button")
     removebutton.classList.add("btn", "btn-danger", "m-2")
     removebutton.innerHTML = '<i class="fa-regular fa-trash-can"></i>'
     removebutton.addEventListener("click", () => {
-        if (confirm("Sei sicuro di voler eliminare il prodotto?")){
+        if (confirm("Sei sicuro di voler eliminare questo prodotto?")){
           deleteProduct(_id)
         }; 
         })
 
     cellActions.append(editbutton, removebutton)
-    RigaUtente.append(NameUs, descriptionUs, priceUs, brandUs, cellActions)
+    RigaUtente.append(NameUs, descriptionUs,brandUs, priceUs, cellActions)
     
      return RigaUtente;
 }; 
 
+//DELETE
 async function deleteProduct(id) {
   try {
       const res = await fetch(linkAPI + id, {
@@ -79,7 +89,46 @@ async function deleteProduct(id) {
       tab(); 
   }
   catch(err) {console.log(err)}; 
-}
+};
+
+//CREARE NUOVO PRODOTTO NELLA TABELLA 
+async function createProduct() {
+    if (inputName.value && inputBrand.value && inputPrice.value && inputImage.value && inputDesc.value) {
+        
+        try {
+            const newProduct = {
+                imageUrl: inputImage.value,
+                name: inputName.value,
+                description: inputDesc.value,
+                brand: inputBrand.value,
+                price: inputPrice.value,
+            }
+
+            const res = await fetch (linkAPI, {
+                method: "POST",
+                body: JSON.stringify(newProduct),
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2N2JlMGUxNDFlMTQwNjAwMTUzMTRkNjkiLCJpYXQiOjE3NDA1MDg2OTIsImV4cCI6MTc0MTcxODI5Mn0.DpwITaKCU7rZ2Fv20-5UysSwfeXKtUy3JTj_pXqAMyA"
+                }
+            })
+            successAlert.classList.remove("d-none")
+            setTimeout(() => {
+                successAlert.classList.add("d-none")
+            }, 4000)
+
+            await tab(); 
+        }
+        catch(err) {
+            console.log(err)
+        }
+    } else {
+        failureAlert.classList.remove("d-none")
+        setTimeout(() => {
+            failureAlert.classList.add("d-none")
+        }, 4000)
+    }
+};
 
 tab(); 
  
---
+++ edit.js
@@ -0,0 +1,115 @@
+const productUrl = "https://striveschool-api.herokuapp.com/api/product/"
+
+// DOM Elements
+
+const inputName = document.getElementById("inputName")
+const inputBrand = document.getElementById("inputBrand")
+const inputPrice = document.getElementById("inputPrice")
+const inputImage = document.getElementById("inputImg")
+const inputDesc = document.getElementById("inputDesc")
+const failureAlert = document.getElementById("failure")
+const successAlert = document.getElementById("success")
+const currentProduct = document.getElementById("currentProduct")
+const returnBtn = document.getElementById("return")
+
+// fetch current product with query string
+
+const urlParams = new URLSearchParams(window.location.search);
+const productId = urlParams.get('id');
+
+function getCurrentProduct() {
+    fetch(productUrl + productId, {
+        headers: {Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2N2JlMGUxNDFlMTQwNjAwMTUzMTRkNjkiLCJpYXQiOjE3NDA1MDg2OTIsImV4cCI6MTc0MTcxODI5Mn0.DpwITaKCU7rZ2Fv20-5UysSwfeXKtUy3JTj_pXqAMyA"}
+    })
+    .then(res => res.json())
+    .then(product => {
+        console.log(product)
+        renderCurrentProduct(product)
+        return product
+    }
+    )
+    .catch(err => console.log(err))
+}
+
+// Render current product
+
+function renderCurrentProduct({name, description, imageUrl, price}) {
+    const card = document.createElement("div")
+    card.classList.add("card", "bg-transparent", "border-0")
+
+    const cardImg = document.createElement("img")
+    cardImg.classList.add("img-fluid", "img-thumbnail")
+    cardImg.src = imageUrl
+
+    const cardBody = document.createElement("div")
+    cardBody.classList.add("card-body")
+
+    const cardTitle = document.createElement("h4")
+    cardTitle.classList.add("card-title")
+    cardTitle.innerText = name
+
+    const cardDescription = document.createElement("p")
+    cardDescription.classList.add("card-text")
+    cardDescription.innerText = description
+
+    const cardPrice = document.createElement("p")
+    cardPrice.classList.add("card-text")
+    cardPrice.innerText = price + "€"
+
+    cardBody.append(cardTitle, cardDescription, cardPrice)
+    card.append(cardImg, cardBody)
+    currentProduct.appendChild(card)
+
+    return card
+}
+
+// Edit products
+
+function editProduct() {
+    
+    const name = inputName.value;
+    const brand = inputBrand.value;
+    const price = inputPrice.value;
+    const imageUrl = inputImage.value;
+    const description = inputDesc.value;
+
+    if (!name || !brand || !price || !imageUrl || !description) {
+        failureAlert.classList.remove("d-none");
+        setTimeout(() => {
+            failureAlert.classList.add("d-none");
+        }, 4000);
+        return; 
+    }
+
+    currentProduct.innerHTML = "";
+    
+    const product = {name, description, imageUrl, price, brand};
+
+    fetch(productUrl + productId, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2N2JlMGUxNDFlMTQwNjAwMTUzMTRkNjkiLCJpYXQiOjE3NDA1MDg2OTIsImV4cCI6MTc0MTcxODI5Mn0.DpwITaKCU7rZ2Fv20-5UysSwfeXKtUy3JTj_pXqAMyA"
+        },
+        body: JSON.stringify(product)
+    })
+    .then(res => res.json())
+    .then(updatedProduct => {
+        
+        successAlert.classList.remove("d-none");
+        setTimeout(() => {
+            successAlert.classList.add("d-none");
+        }, 4000);
+    
+        renderCurrentProduct(updatedProduct);
+    })
+    .catch(err => console.error(err));
+}
+
+
+
+getCurrentProduct()
+
+returnBtn.addEventListener("click", () => {
+    window.location.href = `backOffice.html`
+})
